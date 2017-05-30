a742664b26fd032eed3dcae6a2565217ddd06b57
Juan <Juan@Juan-Notebook>
2017-05-29 21:03:46 -0300

    Working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title : string = 'Vista de Ventas';
+  infoParaElProfe : string = 'Profe:' 
+      +' Lo que hago en la web es presentarle a cada vendedor las ventas realizadas.'
+      +' Para acceder a ellas debe ingresar usuario y contraseña.'
+      +' Los usuarios y las contraseñas válidas están en el link del JSON. '
+      +' El sistema es simple pero todo el código fue escrito de cero al igual que los estilos aplicados. '
+      +' Los datos son tomados por HTTP desde un JSON que yo he creado previamente en forma pública. ';
+  ;
+ 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,26 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { BotonSiNoComponent } from './boton-si-no/boton-si-no.component';
+import { ListadoPreguntasComponent } from './listado-preguntas/listado-preguntas.component';
+import { LoginComponent } from './login/login.component';
+import { UsuariosService, Usuario } from './login/usuarios.service';
+import { VentasComponent } from './ventas/ventas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BotonSiNoComponent,
+    ListadoPreguntasComponent,
+    LoginComponent,
+    VentasComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [UsuariosService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
